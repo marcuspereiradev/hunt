4598e7ecd071c03421f8be146d2100813cd8099e
marcuspereiradev <marcus.work@hotmail.com>
2019-02-25 10:34:59 -0300

    Infinity Scroll

--- src/pages/main.js
+++ src/pages/main.js
@@ -11,15 +11,27 @@ export default class Main extends Component {
     headerTintColor: '#fff',
   }
   state = {
+    productInfo: {},
     docs: [],
+    page: 1,
   }
   componentDidMount() {
     this.loadProducts();
   }
-  loadProducts = async () => {
-    const response = await api.get("/products");
-    const { docs } = response.data;
-    this.setState({ docs });
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`);
+    const { docs, ...productInfo } = response.data;
+    this.setState({ 
+      docs: [...this.state.docs, ...docs],
+      productInfo,
+      page
+    });
+  }
+  loadMore = () => {
+    const { page, productInfo } = this.state;
+    if (page === productInfo.pages) return;
+    const pageNumber = page + 1;
+    this.loadProducts(pageNumber);
   }
   renderItem = ({ item }) => (
     <View style={styles.productContainer}>
@@ -38,6 +50,8 @@ export default class Main extends Component {
           data={this.state.docs}
           keyExtractor={item => item._id}
           renderItem={this.renderItem}
+          onEndReached={this.loadMore}
+          onEndReachedThreshold={0.1}
         />
       </View>
     );
--- src/routes.js
+++ src/routes.js
@@ -2,5 +2,5 @@ import { createStackNavigator } from "react-navigation";
 import Main from './pages/main';
 
 export default createStackNavigator({
-    Main,
+  Main,
 });
