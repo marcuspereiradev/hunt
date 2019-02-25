90fa022844605725216539d4caa42bab44b2b216
marcuspereiradev <marcus.work@hotmail.com>
2019-02-25 11:58:16 -0300

    WebView details

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 import { createStackNavigator } from "react-navigation";
 import Main from './pages/main';
+import Product from './pages/product'
 
 export default createStackNavigator({
   Main,
+  Product,
 });
